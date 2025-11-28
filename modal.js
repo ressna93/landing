23528e6fc645059e6064a52ae2d1094494b531da
www1240kr-web <www.1240.kr@gmail.com>
2025-11-28 09:02:08 +0900

    feat: subscribe-section::after,  fix: /main/ text 미디어쿼리 +모달 js파일 추가

---
+++ modal.js
@@ -0,0 +1,22 @@
+// 필요한 요소 선택
+const modal = document.getElementById("my-modal");
+const subscribeBtn = document.getElementById("subscribe-btn");
+const closeBtn = document.querySelector("modal-close-btn");
+
+// 1. Subscribe 버튼 클릭 시 모달 표시
+subscribeBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // 폼 제출 방지
+  modal.classList.remove("hidden");
+});
+
+// 2. 닫기 버튼 클릭 시 모달 숨김
+closeBtn.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
+
+// 3. 오버레이 클릭 시 모달 숨김
+modal.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.classList.add("hidden");
+  }
+});
