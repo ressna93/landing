7e883d468b243c802981495ab724f7c7ab63081d
www1240kr-web <www.1240.kr@gmail.com>
2025-11-28 10:42:49 +0900

    fix: 모달창 닫기 기능(오버레이 클릭) 오류 수정

--- modal.js
+++ modal.js
@@ -1,7 +1,6 @@
 // 필요한 요소 선택
 const modal = document.getElementById("my-modal");
 const subscribeBtn = document.getElementById("subscribe-btn");
-const closeBtn = document.querySelector("modal-close-btn");
 
 // 1. Subscribe 버튼 클릭 시 모달 표시
 subscribeBtn.addEventListener("click", function (event) {
@@ -9,12 +8,7 @@ subscribeBtn.addEventListener("click", function (event) {
   modal.classList.remove("hidden");
 });
 
-// 2. 닫기 버튼 클릭 시 모달 숨김
-closeBtn.addEventListener("click", function () {
-  modal.classList.add("hidden");
-});
-
-// 3. 오버레이 클릭 시 모달 숨김
+// 2. 오버레이 클릭 시 모달 숨김
 modal.addEventListener("click", function (event) {
   if (event.target === modal) {
     modal.classList.add("hidden");
